38975ebe13482fe7e25f74dcc47e8d3483dcc26c
Stephan2a <stephan.gueorguieff@upikajob.com>
2023-09-06 15:45:58 +0200

    dl tailwind css

--- www/Views/front.tpl.php
+++ www/Views/front.tpl.php
@@ -1,14 +1,18 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="UTF-8">
     <title>Ma super page</title>
     <meta name="description" content="Ceci est ma super page">
+    <link href="/dist/output.css" rel="stylesheet">
 </head>
-<body>
-<h1>Template de front</h1>
 
-<?php include $this->view; ?>
+<body>
+    <h1 class="">Template de front</h1>
+
+    <?php include $this->view; ?>
 
 </body>
+
 </html>
--- www/index.php
+++ www/index.php
@@ -1,13 +1,14 @@
 <?php
+
 namespace App;
 //Contrainte : utilisation des Namespace
 
 
 spl_autoload_register(function ($class) {
     //Core/View.php
-    $class = str_replace("App\\","", $class);
-    $class = str_replace("\\","/", $class).".php";
-    if(file_exists($class)){
+    $class = str_replace("App\\", "", $class);
+    $class = str_replace("\\", "/", $class) . ".php";
+    if (file_exists($class)) {
         include $class;
     }
 });
@@ -17,10 +18,10 @@ spl_autoload_register(function ($class) {
 //Récupérer dans l'url l'uri /login ou /user/toto
 //Nettoyer la donnée
 //S'il y a des paramètres dans l'url il faut les enlever :
-$uriExploded = explode("?",$_SERVER["REQUEST_URI"]);
-$uri = rtrim(strtolower(trim($uriExploded[0])),"/");
+$uriExploded = explode("?", $_SERVER["REQUEST_URI"]);
+$uri = rtrim(strtolower(trim($uriExploded[0])), "/");
 //Dans le cas ou nous sommes à la racine $uri sera vide du coup je remets /
-$uri = (empty($uri))?"/":$uri;
+$uri = (empty($uri)) ? "/" : $uri;
 
 //Créer un fichier yaml contenant le route du type :
 // /login:
@@ -38,51 +39,43 @@ $uri = (empty($uri))?"/":$uri;
 // $controller = new Security();
 // $controller->login();
 
-if(!file_exists("routes.yml")) {
+if (!file_exists("routes.yml")) {
     die("Le fichier de routing n'existe pas");
 }
 
 $routes = yaml_parse_file("routes.yml");
 
 //Page 404
-if(empty($routes[$uri])) {
+if (empty($routes[$uri])) {
     die("Page 404");
 }
 
-if(empty($routes[$uri]["controller"]) || empty($routes[$uri]["action"])) {
-    die("Absence de controller ou d'action dans le ficher de routing pour la route ".$uri);
+if (empty($routes[$uri]["controller"]) || empty($routes[$uri]["action"])) {
+    die("Absence de controller ou d'action dans le ficher de routing pour la route " . $uri);
 }
 
 $controller = $routes[$uri]["controller"];
 $action = $routes[$uri]["action"];
 
 //Vérification de l'existance de la classe
-if(!file_exists("Controllers/".$controller.".php")){
-    die("Le fichier Controllers/".$controller.".php n'existe pas");
+if (!file_exists("Controllers/" . $controller . ".php")) {
+    die("Le fichier Controllers/" . $controller . ".php n'existe pas");
 }
 
-include "Controllers/".$controller.".php";
+include "Controllers/" . $controller . ".php";
 
 //Le fichier existe mais est-ce qu'il possède la bonne classe
 //bien penser à ajouter le namespace \App\Controllers\Security
-$controller = "\\App\\Controllers\\".$controller;
-if(!class_exists($controller)){
-    die("La class ".$controller." n'existe pas");
+$controller = "\\App\\Controllers\\" . $controller;
+if (!class_exists($controller)) {
+    die("La class " . $controller . " n'existe pas");
 }
 
 $objet = new $controller();
 
 //Est-ce que l'objet contient bien la methode
-if(!method_exists($objet, $action)){
-    die("L'action ".$action." n'existe pas");
+if (!method_exists($objet, $action)) {
+    die("L'action " . $action . " n'existe pas");
 }
 
 $objet->$action();
-
-
-
-
-
-
-
-
